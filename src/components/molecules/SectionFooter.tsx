dc0953c2f7a7ec3ed80d2db8351dafb634fa7815
aserlite <arthurcuvillon691@gmail.com>
2026-05-05 16:20:18 +0200

    chore(release): v1.3.0

--- src/components/molecules/SectionFooter.tsx
+++ src/components/molecules/SectionFooter.tsx
@@ -86,7 +86,7 @@ export default function SectionFooter() {
           variant="caption"
           style={[styles.versionText, { color: colors.text }]}
         >
-          v1.2.1 - 2026 Ferme du Buisson©
+          v1.3.0 - 2026 Ferme du Buisson©
         </Typography>
       </View>
     </View>
