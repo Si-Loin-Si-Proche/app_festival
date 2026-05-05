cb652f1f84babf034563e0b2c16aa838e065d5c4
aserlite <arthurcuvillon691@gmail.com>
2026-05-05 16:45:03 +0200

    chore(release): v1.3.3

--- src/screens/Reglages.tsx
+++ src/screens/Reglages.tsx
@@ -115,7 +115,7 @@ export default function ReglagesScreen() {
             variant="caption"
             style={{ textAlign: 'center', opacity: 0.5 }}
           >
-            v1.3.2 - 2026 Ferme du Buisson©
+            v1.3.3 - 2026 Ferme du Buisson©
           </Typography>
         </View>
       </ScrollView>
